4182ed65ecf46f978748790e965538fd2d30c516
fengor wolfsclaw <fengor@fengors-realm.de>
2016-12-11 22:27:49 +0100

    added beancount

--- enable.sh
+++ enable.sh
@@ -1,21 +1,21 @@
 rm -f ~/.bashrc
-ln -s bash/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 
 rm -rf ~/.i3 
-ln -s i3 ~/.i3
+ln -s ~/dotfiles/i3 ~/.i3
 
 rm -f ~/.i3status.conf
-ln -s i3status.conf ~/.i3status.conf
+ln -s ~/dotfiles/i3status.conf ~/.i3status.conf
 
 rm -rf ~/.vim
-ln -s vim ~/.vim
+ln -s ~/dotfiles/vim ~/.vim
 rm -f ~/.vimrc
-ln -s vim/essential.vim ~/.vimrc
+ln -s ~/dotfiles/vim/essential.vim ~/.vimrc
 
 rm -rf ~/.mutt
-ln -s mutt ~/.mutt
+ln -s ~/dotfiles/mutt ~/.mutt
 rm -f ~/.msmtprc
-ln -s msmtp/msmtprc ~/.msmtprc
+ln -s ~/dotfiles/msmtp/msmtprc ~/.msmtprc
 
 rm ~/.Xresources
-ln -s Xresources ~/.Xresources
+ln -s ~/dotfiles/Xresources ~/.Xresources
