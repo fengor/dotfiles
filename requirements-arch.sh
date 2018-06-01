b7997f4ecd531b95ea85413d4931636b4fceeb21
Daniel Brachmann <github@fengors-realm.de>
2018-06-01 09:32:42 +0200

    add some more utility packages

--- requirements-arch.sh
+++ requirements-arch.sh
@@ -1,1 +1,1 @@
-sudo pacman -S mutt msmtp offlineimap notmuch gnupg vim i3 rxvt-unicode xdg-utils
+sudo pacman -S mutt msmtp offlineimap notmuch gnupg vim i3 rxvt-unicode xdg-utils fzf the_silver_searcher tmux
